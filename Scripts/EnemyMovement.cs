a77de44089a2858f78567e839095bdde1397a1a6
Jingnan Xiao <43525888+Xiaojnconcordia@users.noreply.github.com>
2020-11-12 14:48:27 -0500

    Add files via upload

---
+++ Scripts/EnemyMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Transform target;
+    private int waypointIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = WayPoints.points[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Vector3 dir = (target.position - transform.position).normalized;
+        transform.Translate(dir * speed * Time.deltaTime);
+
+        if (Vector3.Distance(target.position, transform.position) < 0.4f)
+        {
+            MoveToNextPoint();
+        }
+
+    }
+
+    void MoveToNextPoint()
+    {
+        if (waypointIndex >= WayPoints.points.Length - 1)
+        {
+            Destroy(gameObject); //when enemy reaches the end, we destroy the enemy object
+            return;
+        }
+
+        waypointIndex++;
+        target = WayPoints.points[waypointIndex];
+
+    }
+}
---
+++ Scripts/WayPoints.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+
+public class WayPoints : MonoBehaviour
+{
+    public static Transform[] points;
+
+    void Awake()
+    {
+        points = new Transform[transform.childCount];
+
+        for (int i = 0; i < points.Length; i++)
+        {
+            points[i] = transform.GetChild(i); //store all the children's Transform in the array
+        }
+
+    }
+
+}
